f67ba3eb2ed654df0a0d5688797bebe4f76504c8
admin <admin@admins-MacBook-Air.local>
2024-04-20 00:33:32 -0700

    В блоке с комментариями добавить возможность оставлять комментарий юзерам - добавить поле для текста, кнопку “опубликовать”

--- assets/custom.js
+++ assets/custom.js
@@ -7,63 +7,73 @@ function handleSubmit(event) {
   const form = event.target;
   const comment = form.elements.userComment.value;
 
-  console.log(event);
-  console.log(form.elements.userComment.value);
-  console.log("Button was clicked");
+  const checkingTheFieldForEmptiness =
+    comment !== ""
+      ? creatMarkapCommentUser(comment)
+      : alert("preencha os campos");
+  form.reset();
 }
 
-const date = new Date().getMinutes();
-console.log(date);
-
 function creatMarkapCommentUser(comment) {
-  const timeWhenTheCommentWasLeftInMinutes = new Date().getMinutes();
+  console.log(comment);
 
+  const word = "fuck";
   const markup = `<div class="comments" id="comment0" style="display: block">
-    <div class="profile">
-      <img src="assets/Images/1.gif" />
-    </div>
-    <div class="comment-content">
-      <p class="name">
-        <div style="vertical-align: inherit">
-          <p style="vertical-align: inherit">Anonymous</p>
-        </div>
-      </p>
-      <p style="vertical-align: inherit>
-        ${comment}
-      </p>
-    </div>
-    <div class="clr"></div>
-    <div class="comment-status">
-      <span>
-        <font style="vertical-align: inherit">
-          <font style="vertical-align: inherit">Curte·comente</font>
-        </font>
-        <img src="assets/Images/like.png" width="15px" height="15px" />
-        <font style="vertical-align: inherit">
-          <font style="vertical-align: inherit">29</font>
-        </font>
-      </span>
+  <div class="profile">
+    <img src="assets/Images/3.jpg" />
+  </div>
+  <div class="comment-content">
+    <p class="name">
       <font style="vertical-align: inherit">
-        <small>
-          <font style="vertical-align: inherit">·</font>
-        </small>
-
-        <small>
-          <u>
-            <font style="vertical-align: inherit"
-              >4 minutos antes</font
-            >
-          </u>
-        </small>
+        <font style="vertical-align: inherit">Anonymous</font>
       </font>
+    </p>
+    <p style="vertical-align: inherit"> ${comment}</p>
+  </div>
+  <div class="clr"></div>
+  <div class="comment-status">
+    <span>
+      <font style="vertical-align: inherit">
+        <font style="vertical-align: inherit">Curte·comente</font>
+      </font>
+      <img src="assets/Images/like.png" width="15px" height="15px" />
+      <font style="vertical-align: inherit">
+        <font style="vertical-align: inherit">29</font>
+      </font>
+    </span>
+    <font style="vertical-align: inherit">
       <small>
-        <font style="vertical-align: inherit"></font>
+        <font style="vertical-align: inherit">·</font>
+      </small>
+
+      <small>
         <u>
-          <font style="vertical-align: inherit"></font>
+          <font style="vertical-align: inherit"
+            >4 minutos antes</font
+          >
         </u>
       </small>
-    </div>
-  </div> `;
+    </font>
+    <small>
+      <font style="vertical-align: inherit"></font>
+      <u>
+        <font style="vertical-align: inherit"></font>
+      </u>
+    </small>
+  </div>
+</div>`;
+
+  form.insertAdjacentHTML("afterend", markup);
 }
 
 // anonymous
+
+{
+  /* <p>
+<font style="vertical-align: inherit">
+  <font style="vertical-align: inherit"
+    >${comment}</font
+  >
+</font>
+</p> */
+}
